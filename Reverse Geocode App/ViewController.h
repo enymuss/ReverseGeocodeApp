e78c3bee9f61dfb825ca33b0db259a8ae7fe020b
Richard Szczerba <richardszczerba@gmail.com>
2015-05-27 02:01:49 +0100

    Reverse Geocodes properly

--- Reverse Geocode App/ViewController.h
+++ Reverse Geocode App/ViewController.h
@@ -7,9 +7,14 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <CoreLocation/CoreLocation.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController <UITextFieldDelegate>
 
+@property (weak, nonatomic) IBOutlet UITextField *longitiudeTextField;
+@property (weak, nonatomic) IBOutlet UITextField *latitudeTextField;
+@property (weak, nonatomic) IBOutlet UILabel *resultLabel;
 
+- (IBAction)geocodeButton:(id)sender;
 @end
 
--- Reverse Geocode App/ViewController.m
+++ Reverse Geocode App/ViewController.m
@@ -9,10 +9,11 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
 @end
 
 @implementation ViewController
+
+NSString * const kAcceptedCharacterSet = @"-.0123456789";
 
 - (void)viewDidLoad {
     [super viewDidLoad];
@@ -22,6 +23,68 @@
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)geocodeButton:(id)sender {
+    if ([self isDataVaild:self.longitiudeTextField.text] &&
+        [self isDataVaild:self.latitudeTextField.text]) {
+        
+        [self reverseGeocode];
+        
+    } else {
+        [self showAlertView];
+    }
+}
+
+- (void) reverseGeocode {
+    // texti fields have content
+    CLGeocoder *geocoder = [[CLGeocoder alloc] init];
+    CLLocation *location = [[CLLocation alloc] initWithLatitude:self.latitudeTextField.text.doubleValue
+                                                      longitude:self.longitiudeTextField.text.doubleValue];
+    
+    [geocoder reverseGeocodeLocation:location completionHandler:^(NSArray *placemarks, NSError *error) {
+        if (error) {
+            [self showAlertView];
+        } else {
+            CLPlacemark *placemark = placemarks.lastObject;
+            if (placemark.subLocality) {
+                self.resultLabel.text = placemark.subLocality;
+            } else {
+                self.resultLabel.text = @"Still Unknown";
+            }
+        }
+    }];
+}
+
+- (void) showAlertView {
+    UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"Incorrect Data"
+                                                        message:@"Please check all fields"
+                                                       delegate:self
+                                              cancelButtonTitle:@"OK"
+                                              otherButtonTitles: nil];
+    [alertView show];
+}
+
+- (BOOL) isDataVaild:(NSString *) dataString {
+    BOOL isValid = TRUE;
+    
+    // false if it's empty
+    if (dataString.length == 0) {
+        return isValid = FALSE;
+    }
+    
+    // iterate over every character.
+    for (int i = 0; i < dataString.length; i++) {
+        unichar character  = [dataString characterAtIndex:i];
+        NSCharacterSet *accpetedCharacters = [NSCharacterSet characterSetWithCharactersInString:kAcceptedCharacterSet];
+        
+        // if it's not an accpetedCharacter, it's not valid
+        if (![accpetedCharacters characterIsMember:character]) {
+            return isValid = FALSE;
+        }
+    }
+    
+    return isValid;
 }
 
 @end
